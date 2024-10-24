73244f673d9564b1b950dcc45e8ae5e8ed219662
Paulo Bustamante <pbustamante@utpl.edu.ec>
2024-10-24 10:40:46 -0500

    Trabajando con Netbeans

---
+++ ejemplos-IDE/Ejemplo05/src/ejemplo05/Demo10.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejemplo05;
+
+/**
+ *
+ * @author utpl
+ */
+public class Demo10 {
+    
+    public static void main(String[] args) {
+        int valor1 = 10;
+        double valor2 = 3.0;
+        int valor3 = 1;
+        double resultado = valor1 / (valor2 + valor3);
+        System.out.println(resultado);
+        
+        valor3 = 2;
+        resultado = valor1 / (valor2 + valor3);
+        System.out.println(resultado);
+        
+    }
+    
+}
---
+++ ejemplos-IDE/Ejemplo05/src/ejemplo05/Demo11.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejemplo05;
+
+/**
+ *
+ * @author utpl
+ */
+public class Demo11 {
+    public static void main(String[] args) {
+        int valor1 = 10;
+        double valor2 = 3.0;
+        int valor3 = 1;
+        System.out.println(valor1 / (valor2 + valor3));
+    }
+}
